4f51d71e5582a8d3bb4052a2e507f09fb2e6e243
Ahmar <76834051+Ahmarkalam@users.noreply.github.com>
2026-03-09 13:34:34 -0700

    Added gunicorn

--- job_scraper/settings.py
+++ job_scraper/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7cv@0n9l=v&xtqna&sv!wpqxg1)@nm3k=qgmqho&n2j2(y0)m!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 ALLOWED_HOSTS = []
 
